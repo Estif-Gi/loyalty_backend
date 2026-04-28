bb3407433cb5109c5f6dbcd1434f71455ab94d64
deliverygebeta-oss <deliverygebeta@gmail.com>
2026-04-28 17:43:23 +0300

    added / route

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    next();
+});
 
 app.use((req, res, next) => {
     console.log("message: API called ->", req.method, req.originalUrl);
     next();
 });
-app.use("/" , (req , res , next) => {
+app.get("/" , (req , res) => {
     res.send({message:"lets get loyalty started"});
-    next();
 });
 app.use("/api/users" , require("./routes/users"));
 app.use("/api/restaurants" , require("./routes/restaurants"));
 app.use("/api/menus" , require("./routes/menus"));
 app.use("/api/loyalty" , require("./routes/loyalty_programs"));
 app.use("/api/notifications" , require("./routes/notifications"));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    next();
-})
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -34,11 +34,14 @@ app.use((err, req, res, next) => {
 });
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(
-    () => {
+    .then(() => {
         console.log("****Connected to MongoDB****");
-        app.listen(process.env.PORT || 5001);
-    }
-).catch(err => {
-    console.log("//////Failed to connect to MongoDB:///////", err);
-});
+        if (process.env.NODE_ENV !== "production") {
+            app.listen(process.env.PORT || 5001);
+        }
+    })
+    .catch(err => {
+        console.log("//////Failed to connect to MongoDB:///////", err);
+    });
+
+module.exports = app;
