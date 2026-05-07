739e1aaeada56c87e578664bf13d2b460ac62010
deliverygebeta-oss <deliverygebeta@gmail.com>
2026-05-07 16:17:51 +0300

    env bug

--- config/db.js
+++ config/db.js
@@ -24,7 +24,7 @@ async function connectDb() {
             serverSelectionTimeoutMS: 10000
         });
     }
-    
+     
     cachedConnection = await cachedPromise;
     return cachedConnection;
 }
