1120913bfa5a7f806f8adcc5baee711e47d79f63
GebetaEst <estifanos@mulewave.com>
2026-05-21 12:52:14 +0300

    updated for deployment on coolify

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,20 +1,20 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../loyalty-e883f-firebase-adminsdk-fbsvc-8560d84572.json');
+// const serviceAccount = require('../loyalty-e883f-firebase-adminsdk-fbsvc-8560d84572.json');
 
 if (!admin.apps.length) {
-    // const serviceAccount = {
-    //     type: process.env.FIREBASE_TYPE,
-    //     project_id: process.env.FIREBASE_PROJECT_ID,
-    //     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    //     private_key: process.env.FIREBASE_PRIVATE_KEY,
-    //     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    //     client_id: process.env.FIREBASE_CLIENT_ID,
-    //     auth_uri: process.env.FIREBASE_AUTH_URI,
-    //     token_uri: process.env.FIREBASE_TOKEN_URI,
-    //     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
-    //     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-    //     universe_domain: 'googleapis.com'
-    // };
+    const serviceAccount = {
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+        universe_domain: 'googleapis.com'
+    };
 
     try {
         admin.initializeApp({
