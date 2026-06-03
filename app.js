639b9f7a382c1c3cce18207eac79a68f78bfab8f
GebetaEst <estifanos@mulewave.com>
2026-06-03 10:13:11 +0300

    aded 200 calles

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50 // limit each IP to 5 requests per windowMs
+    max: 200 // limit each IP to 5 requests per windowMs
 });
 
 
